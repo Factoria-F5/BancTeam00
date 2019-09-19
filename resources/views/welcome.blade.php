7bd57a5e79e13cf42b802477953086d7af11d702
noemisg <nsoriaga7@gmail.com>
2019-09-19 13:24:35 +0200

    1r pushito lindo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Banc Team
                 </div>
 
                 <div class="links">
